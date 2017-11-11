875ba75d94259a6e117334305c2b4ae74bd447ae
Jina-Kim <jinakimcool@gmail.com>
2017-11-11 16:39:55 +0900

    add actionbar color

--- app/src/main/java/kr/ac/hansung/bababob/MainActivity.java
+++ app/src/main/java/kr/ac/hansung/bababob/MainActivity.java
@@ -2,6 +2,7 @@ package kr.ac.hansung.bababob;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TableLayout;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar);
         setContentView(R.layout.activity_main);
 
         MyPagerAdapter mPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
