7f5f216bbceda4d2f1c00b9a93b741d6ab534109
sorin <jnsorn@gmail.com>
2017-12-02 19:52:37 +0900

    test

--- app/src/main/java/kr/ac/hansung/bababob/MyPagerAdapter.java
+++ app/src/main/java/kr/ac/hansung/bababob/MyPagerAdapter.java
@@ -4,12 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import kr.ac.hansung.bababob.Restaurant.RestaurantFragment;
-import kr.ac.hansung.bababob.SchoolCafeteria.SchoolCafeteriaFragment;
-/**
- * Created by Jina on 2017-11-11.
- */
-
 public class MyPagerAdapter extends FragmentPagerAdapter {
 
     private static int PAGE_NUM = 3;
@@ -30,13 +24,13 @@ public class MyPagerAdapter extends FragmentPagerAdapter {
                 return null;
         }
     }
-    @Override
+
     public String getPageTitle(int position){
         switch (position) {
             case 0:
                 return "Restaurant";
             case 1:
-                return "School Menu";
+                return "School Cafeteria";
             case 2:
                 return "Chatting";
             default:
@@ -47,9 +41,9 @@ public class MyPagerAdapter extends FragmentPagerAdapter {
     public int getIcon(int position){
         switch (position) {
             case 0:
-                return R.drawable.shop;
-            case 1:
                 return R.drawable.restaurant;
+            case 1:
+                return R.drawable.shop;
             case 2:
                 return R.drawable.chat;
             default:
